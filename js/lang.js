ff8b0f51dc189b674cdfb75df898d900caa92283
Milana <milanakhairullina@gmail.com>
2023-08-24 18:43:54 +0300

    add: add all files of project

---
+++ js/lang.js
@@ -0,0 +1,22 @@
+const langArr = {
+	"unit": {
+		"ru": "Добавить город",
+		"en": "Add city",
+	},
+	"pressure": {
+		"ru": "Давление",
+		"en": "Pressure",
+	},
+		"Humidity": {
+		"ru": "Влажность",
+		"en": "Humidity",
+	},
+		"High": {
+		"ru": "Макс.",
+		"en": "High",
+	},
+		"name_city": {
+		"ru": "Др.город",
+		"en": "Add city",
+	},
+}
